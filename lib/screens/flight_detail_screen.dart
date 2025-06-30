ff41aee23870ac69c5068f0ef1a3ab0eb4d4d467
vzeman <vzeman@qualityunit.com>
2025-06-30 06:43:53 +0200

    implemented todos

--- lib/screens/flight_detail_screen.dart
+++ lib/screens/flight_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:intl/intl.dart';
+import 'package:share_plus/share_plus.dart';
 import '../models/flight.dart';
 import '../widgets/altitude_chart.dart';
 import '../widgets/speed_chart.dart';
@@ -120,7 +121,7 @@ class _FlightDetailScreenState extends State<FlightDetailScreen> {
             IconButton(
               icon: const Icon(Icons.share),
               onPressed: () {
-                // TODO: Implement share functionality
+                _shareFlightData();
               },
             ),
           ],
@@ -241,6 +242,34 @@ class _FlightDetailScreenState extends State<FlightDetailScreen> {
         ),
       ),
     );
+  }
+
+  void _shareFlightData() {
+    final flight = widget.flight;
+
+    // Calculate min altitude from altitudes list
+    final minAltitude = flight.altitudes.isNotEmpty
+        ? flight.altitudes.reduce((a, b) => a < b ? a : b)
+        : 0.0;
+
+    // Prepare the flight data for sharing
+    final StringBuffer sb = StringBuffer();
+    sb.writeln('Flight Details:');
+    sb.writeln('Date: ${DateFormat('MMM d, y').format(flight.startTime)}');
+    sb.writeln('Duration: ${_formatDuration(flight.duration)}');
+    sb.writeln('Distance: ${(flight.distanceTraveled / 1000).toStringAsFixed(1)} km');
+    sb.writeln('Max Speed: ${(flight.maxSpeed * 3.6).toStringAsFixed(1)} km/h');
+    sb.writeln('Avg Speed: ${(flight.averageSpeed * 3.6).toStringAsFixed(1)} km/h');
+    sb.writeln('Max Altitude: ${flight.maxAltitude.toStringAsFixed(0)} m');
+    sb.writeln('Min Altitude: ${minAltitude.toStringAsFixed(0)} m');
+    sb.writeln('Points: ${flight.path.length}');
+    sb.writeln('Recording Started: ${_formatZuluTime(flight.recordingStartedZulu)}');
+    if (flight.recordingStoppedZulu != null) {
+      sb.writeln('Recording Stopped: ${_formatZuluTime(flight.recordingStoppedZulu!)}');
+    }
+
+    // Share the flight data
+    Share.share(sb.toString(), subject: 'Flight Details');
   }
 
   Widget _buildMap() {
